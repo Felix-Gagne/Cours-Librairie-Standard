351be3210760c503d23b087932c740a2ff49f935
felix <2070094@cegepmontpetit.ca>
2021-09-14 10:45:31 -0400

    EntrezUnNombre
    Exercice EntrezUnNombre

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 /**
@@ -11,6 +12,19 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("Veuillez entrer un nombre:");
+        boolean done = false;
+        while(!done){
+            try {
+                Scanner scan = new Scanner(System.in);
+                int input = scan.nextInt();
+                System.out.println("Merci votre nombre est " + input);
+                done = true;
+            }
+            catch (Exception e) {
+                System.out.println("Ceci n'est pas un nombre entier, veuillez entrer un nombre entier:");
 
+            }
+        }
     }
 }
